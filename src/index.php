be8847815eef874e5acd1d907fd160ca177c94d6
Kristof Grenson <kristof@nizer.be>
2024-03-04 22:22:53 +0100

    htaccess fix

--- src/index.php
+++ src/index.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // DATABASE INFO
 // ----------------------------
 define("DB_HOST", "com-linweb575.srv.combell-ops.net");
